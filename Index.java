d064407b104d9c894ebee424abf8c67b4daf5a6e
pratheba17 <prathebait@gmail.com>
2017-06-28 22:01:11 +0530

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Index
+{
+    public static void Index(int b[])
+    {
+        Arrays.sort(b);
+        for(int i=0;i<b.length;i++)
+        {
+            if(i==b[i])
+            {
+                System.out.print(b[i]+" ");
+            }
+        }
+    } 
+    public static void main(String args[])
+   {
+         Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         int c[]=new int[n];
+         for(int i=0;i<n;i++)
+         {
+             c[i]=in.nextInt();
+         }
+         Index(c);
+    }
+}

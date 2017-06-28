a8dd0615b39034e2c846db762c7fef7d8eb1da0a
pratheba17 <prathebait@gmail.com>
2017-06-28 10:14:01 +0530

    Create reversenum.java

---
+++ reversenum.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class reversenum
+{
+    public static void main(String args[])
+    {
+      int num=0;
+      int reversenum =0;
+      Scanner in = new Scanner(System.in);
+      num = in.nextInt();
+      while( num != 0 )
+      {
+          reversenum = reversenum * 10;
+          reversenum = reversenum + num%10;
+          num = num/10;
+      }
+
+      System.out.println("Reverse of input number is: "+reversenum);
+	}
+}

b5ac63328aa913b05eba231408d36b174cc391c8
pratheba17 <prathebait@gmail.com>
2017-07-09 17:37:15 +0530

    Create WorkingDay.java

---
+++ WorkingDay.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class WorkingDay {
+
+	public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       String str=sc.next();
+       if(str.equals("sunday")|| str.equals("saturday")){
+    	   System.out.println("false");
+       }
+       else{
+    	   System.out.println("True");
+       }
+       sc.close();
+	}
+}

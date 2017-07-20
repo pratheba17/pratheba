ae9296100877499e61099dc86397289c73bb8ae5
pratheba17 <prathebait@gmail.com>
2017-07-20 21:17:55 +0530

    Create secsmall.java

---
+++ secsmall.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class secsmall
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
